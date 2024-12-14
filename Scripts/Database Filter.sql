ac6e814442f47f71c37db7208abd352672ed4324
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2024-12-14 16:32:11 +0000

    Update Database Filter.sql

--- Scripts/Database Filter.sql
+++ Scripts/Database Filter.sql
@@ -35,8 +35,37 @@ WHERE Stx = '-'
 
 -- Certifying that missing entries == only "Stx" -- 
 
-DELETE FROM metadata
-WHERE Stx = '-' AND id IS NOT NULL;
+-- CREATE TABLE metadata_region_2 (
+-- Region TEXT,
+-- Counter INTEGER
+-- );
 
-SELECT * FROM 
-metadata;
+DELIMITER $$ -- Temporary delimiter for ";" accuracy --
+
+CREATE TRIGGER limit_metadata_rows
+BEFORE INSERT ON metadata_region_2
+FOR EACH ROW
+BEGIN
+	IF (SELECT COUNT(*) FROM metadata_region_2) >= 11 THEN
+    SIGNAL SQLSTATE "45000" -- Custom SQL eror display -- 
+    SET MESSAGE_TEXT  = "Max row number exceeded";
+END IF;
+END$$
+
+DELIMITER $$;
+    
+INSERT INTO metadata_region_2 (Region, Counter)
+VALUES 
+	("UK", 2174),
+    ("Australasia", 6),
+    ("C. America", 29),
+    ("C.Europe", 62),
+    ("M.East", 157),
+    ("N. Africa", 91),
+    ("N. America", 12),
+    ("N. Europe", 18),
+    ("S. America", 3),
+    ("S. Europe", 237),
+    ("Subsaharan Africa", 28);
+    
+SELECT * FROM metadata_region_2;
