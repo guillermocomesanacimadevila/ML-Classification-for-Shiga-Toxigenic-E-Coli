60c9430c8f00d9414b085ded755d40745171611d
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2024-12-14 16:56:21 +0000

    Update Database Filter.sql

--- Scripts/Database Filter.sql
+++ Scripts/Database Filter.sql
@@ -1,6 +1,6 @@
 -- SELECT * FROM 
 -- metadata;
--- Analysis conducted in MySQL
+-- Analysis conducted in a MySQL Workbench server
 
 SELECT COUNT(*)
 FROM metadata
@@ -47,7 +47,7 @@ CREATE TRIGGER limit_metadata_rows
 BEFORE INSERT ON metadata_region_2
 FOR EACH ROW
 BEGIN
-	IF (SELECT COUNT(*) FROM metadata_region_2) >= 11 THEN
+	IF (SELECT COUNT(*) FROM metadata_region_2) >= 12 THEN
     SIGNAL SQLSTATE "45000" -- Custom SQL eror display -- 
     SET MESSAGE_TEXT  = "Max row number exceeded";
 END IF;
@@ -57,7 +57,7 @@ DELIMITER $$;
     
 INSERT INTO metadata_region_2 (Region, Counter)
 VALUES 
-	("UK", 2174),
+    ("UK", 2174),
     ("Australasia", 6),
     ("C. America", 29),
     ("C.Europe", 62),
@@ -67,6 +67,65 @@ VALUES
     ("N. Europe", 18),
     ("S. America", 3),
     ("S. Europe", 237),
-    ("Subsaharan Africa", 28);
+    ("Subsaharan Africa", 28),
+    ("Asia", 57);
     
-SELECT * FROM metadata_region_2;
+SELECT * FROM metadata_region_2; 
+-- Export new table -- 
+
+-- Now update metadata csv file and delete the rows where Stx = "-" -- 
+
+SET SQL_SAFE_UPDATES = 0; -- Disipate Safe Update -- 
+
+DELETE FROM metadata2
+WHERE Stx = "-";
+
+SELECT * FROM metadata2;
+SELECT COUNT(*) FROM metadata2;
+
+SELECT Region, COUNT(*) AS Region_Count
+FROM metadata2
+GROUP BY Region
+ORDER BY Region;
+
+SET SQL_SAFE_UPDATES = 1; -- Disipate Safe Update -- 
+
+-- Final loop to get the final value .csv -- 
+
+-- SELECT * FROM final_table_filtered;
+
+CREATE TABLE filtered_values_2 (
+Region TEXT,
+Counter INTEGER
+);
+
+DELIMITER $$ -- Temporary delimiter for ";" accuracy --
+
+CREATE TRIGGER filtered_metadata_rows
+BEFORE INSERT ON filtered_values_2
+FOR EACH ROW
+BEGIN
+	IF (SELECT COUNT(*) FROM filtered_values_2) >= 12 THEN
+    SIGNAL SQLSTATE "45000" -- Custom SQL eror display -- 
+    SET MESSAGE_TEXT  = "Max row number exceeded";
+END IF;
+END$$
+
+DELIMITER $$;
+
+INSERT INTO filtered_values_2 (Region, Counter)
+VALUES 
+    ("UK", 2121),
+    ("Australasia", 6),
+    ("C. America", 29),
+    ("C.Europe", 58),
+    ("M.East", 154),
+    ("N. Africa", 89),
+    ("N. America", 11),
+    ("N. Europe", 18),
+    ("S. America", 3),
+    ("S. Europe", 235),
+    ("Subsaharan Africa", 28),
+    ("Asia", 55);
+    
+SELECT * FROM filtered_values_2;
