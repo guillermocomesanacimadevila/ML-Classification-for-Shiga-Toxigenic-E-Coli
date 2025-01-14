3db95ebb6b7c68bbb56e6e75d1a566e06a1d824e
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-01-14 16:27:33 +0000

    Update more visualisations.R

--- Scripts/more visualisations.R
+++ Scripts/more visualisations.R
@@ -11,12 +11,20 @@ library(tidyr)
 library(remotes)
 library(ggpubr)
 
+
 setwd("/Users/guillermocomesanacimadevila/Desktop/(MSc) BIOINFORMATICS/APPLIED DATA SCIENCE IN BIOLOGY/Coursework/Applied Data Science CW 1 (Script)/Scripts/Counter_tables")
 
-region_counters <- read.csv("region_counters.csv", header = TRUE)
-country_counters <- read.csv("country_counters.csv", header = TRUE)
-stx_counters <- read.csv("stx_counters.csv", header = TRUE)
-pt_counters <- read.csv("pt_counters.csv", header = TRUE)
+metadata_cleaned <- read.csv("cleaned_table_final.csv", header = TRUE)
+region_counters <- read.csv("cleaned_region.csv", header = TRUE)
+country_counters <- read.csv("cleaned_country.csv", header = TRUE)
+stx_counters <- read.csv("cleaned _stx.csv", header = TRUE)
+pt_counters <- read.csv("cleaned_pt.csv", header = TRUE)
+
+dim(metadata_cleaned)
+dim(region_counters)
+dim(country_counters)
+dim(stx_counters)
+dim(pt_counters)
 
 custom_theme <- theme_minimal(base_size = 12) + 
   theme(
@@ -33,28 +41,28 @@ p1 <- ggplot(region_counters, aes(x = reorder(Region, desc(Region_Count)), y = R
   geom_bar(stat = "identity", position = position_dodge(), color = "lightblue", fill = "steelblue") +
   labs(title = "Region-wide Sample Distribution", x = "Region", y = "Samples") +
   removeGrid(x = FALSE, y = FALSE)
-  custom_theme
+custom_theme
 
 # Country bar chart
 p2 <- ggplot(country_counters, aes(x = reorder(Country, desc(Country_Count)), y = Country_Count, fill = Country_Count)) +
   geom_bar(stat = "identity", position = position_dodge(), color = "lightblue", fill = "steelblue") +
   labs(title = "Region-wide Sample Distribution", x = "Region", y = "Samples") +
   removeGrid(x = FALSE, y = FALSE)
-  custom_theme
+custom_theme
 
 # Stx bar chart
 p3 <- ggplot(stx_counters, aes(x = reorder(Stx, desc(Stx_Count)), y = Stx_Count, fill = Stx_Count)) +
   geom_bar(stat = "identity", position = position_dodge(), color = "lightblue", fill = "steelblue") +
   labs(title = "Region-wide Sample Distribution", x = "Region", y = "Samples") +
   removeGrid(x = FALSE, y = FALSE)
-  custom_theme
+custom_theme
 
 # PT Bar chart  
 p4 <- ggplot(pt_counters, aes(x = reorder(PT, desc(PT_Count)), y = PT_Count, fill = PT_Count)) +
   geom_bar(stat = "identity", position = position_dodge(), color = "lightblue", fill = "steelblue") +
   labs(title = "Region-wide Sample Distribution", x = "Region", y = "Samples") +
   removeGrid(x = FALSE, y = FALSE)
-  custom_theme
+custom_theme
 
 # ===== Multi-panneled figuremn ===== #
 multi_paneled_figure <- ggarrange(
