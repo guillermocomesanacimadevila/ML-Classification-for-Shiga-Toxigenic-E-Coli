c437ea22126e7ff5d1d46ff75f8d6bda2989ba1d
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-01-14 16:19:36 +0000

    Update data cleaning.sql

--- Scripts/data cleaning.sql
+++ Scripts/data cleaning.sql
@@ -1,26 +1,6 @@
 -- SELECT COUNT(*)
 -- FROM metadata_f;
 
-SELECT Region, COUNT(*) as Region_Count
-FROM metadata_f
-GROUP BY Region
-ORDER BY Region_Count DESC;
-
-SELECT Country, COUNT(*) as Country_Count
-FROM metadata_f
-GROUP BY Country
-ORDER BY Country_Count DESC;
-
-SELECT Stx, COUNT(*) as Stx_Count
-FROM metadata_f
-GROUP BY Stx
-ORDER BY Stx_Count DESC;
-
-SELECT PT, COUNT(*) AS PT_Count
-FROM metadata_f
-GROUP BY PT
-ORDER BY PT_Count Desc;
-
 SELECT Country, COUNT(*) as Country_count -- Portugal!!!
 FROM metadata_f
 GROUP BY Country
@@ -43,3 +23,38 @@ WHERE Stx <> '-'
   AND Stx <> 'NA'
   AND Country <> "Portgual"
 ORDER BY Region;
+
+-- # Export relevant tables for analyses # --
+
+SELECT * 
+FROM metadata_f
+WHERE Stx <> '-' 
+  AND PT NOT IN ('untypable', '#N/A', 'NA')
+  AND Stx <> 'NA'
+  AND Country <> "Portgual";
+
+-- # Create new tables from cleaned dataset # --
+-- metadata_cleaned_f
+-- Region
+SELECT Region, COUNT(*) as Region_Count
+FROM metadata_cleaned_f
+GROUP BY Region
+ORDER BY Region_Count DESC;
+
+-- Country
+SELECT Country, COUNT(*) as Country_Count
+FROM metadata_cleaned_f
+GROUP BY Country
+ORDER BY Country_Count DESC;
+
+-- Stx
+SELECT Stx, COUNT(*) as Stx_Count
+FROM metadata_cleaned_f
+GROUP BY Stx
+ORDER BY Stx_Count DESC;
+
+-- PT
+SELECT PT, COUNT(*) AS PT_Count
+FROM metadata_cleaned_f
+GROUP BY PT
+ORDER BY PT_Count Desc;
